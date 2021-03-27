7b3455e62b7ad5f5f33811db005aebd8a8b09222
Oksana Tugusheva <gutuxa@gmail.com>
2021-03-27 13:47:22 +0300

    HW_2.2 color picker

--- ColorPickerApp/ViewController.swift
+++ ColorPickerApp/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Color View
+        colorView.layer.borderWidth = 1
+        colorView.layer.borderColor = UIColor.systemGray3.cgColor
+        colorView.layer.cornerRadius = 10
+        
+        mixColor()
     }
 
-
+    @IBAction func redSliderChanged() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        mixColor()
+    }
+    
+    @IBAction func greenSliderChanged() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        mixColor()
+    }
+    
+    @IBAction func blueSliderChanged() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        mixColor()
+    }
+    
+    
+    private func mixColor() {
+        colorView.layer.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        ).cgColor
+    }
 }
 
